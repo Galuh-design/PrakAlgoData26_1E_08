3a37136634a99dafaf67b045df8f13c8f16ad2fa
Galuh-design <pramudyaanantagaluh@gmail.com>
2026-02-26 11:14:54 +0700

    Modifikasi Percobaan 3 Pertanyaan1

--- minggu3/Matakuliah08.java
+++ minggu3/Matakuliah08.java
@@ -1,12 +1,37 @@
 package minggu3;
-
+import java.util.Scanner;
 public class Matakuliah08 {
     public String kode;
     public String nama;
-    public int sks, jumlahJam ;
-  
+    String dummy;
+    public int sks, jumlahJam;
+    public int iNO = 1;
+    Scanner galuh = new Scanner(System.in);
+    
+    
+    public void tambahData (){
+        System.out.println("Masukkan Data MataKuliah ke-" +iNO);
+            System.out.print("Kode        :   ");
+            kode = galuh.nextLine();
+            System.out.print("Nama        :   ");
+            nama = galuh.nextLine();
+            System.out.print("Sks         :   ");
+            dummy = galuh.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah Jam  :   ");
+            dummy = galuh.nextLine();
+            jumlahJam = Integer.parseInt(dummy);
+            System.out.println("--------------------------------");
+            iNO +=1;
 
+    }
+
+
+
+
+    public Matakuliah08(){}
     public Matakuliah08(String kode, String nama, int sks, int jumlahJam) {
+        
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
