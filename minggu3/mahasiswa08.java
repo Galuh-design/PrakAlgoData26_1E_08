600ac056d85e4187eca44929193cf4ece5702800
Galuh-design <pramudyaanantagaluh@gmail.com>
2026-02-26 09:11:09 +0700

    modifikasi percobaan 2 pertanyaan 1

--- minggu3/mahasiswa08.java
+++ minggu3/mahasiswa08.java
@@ -1,8 +1,19 @@
 package minggu3;
 
 public class mahasiswa08 {
-public String nim;
-public String nama;
-public String kelas;
-public float ipk;
+
+    public String nim;
+    public String nama;
+    public String kelas;
+    public float ipk;
+
+    public void cetakInfo(String NIM, String name, String kelas, float Ipk) {
+        System.out.println("NIM     : " + NIM);
+        System.out.println("Nama    : " + name);
+        System.out.println("Kelas   : " + kelas);
+        System.out.println("IPK     : " + Ipk);
+        System.out.println("---------------------------------");
+
+    }
+
 }
