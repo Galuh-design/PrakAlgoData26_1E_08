e70b8b5779a35d4dfc7ac4a5972534e792607338
Galuh-design <pramudyaanantagaluh@gmail.com>
2026-03-12 08:10:36 +0700

    modifikasi percobaan2 pertanyaan 3

--- minggu5/MainPangkat08.java
+++ minggu5/MainPangkat08.java
@@ -19,7 +19,7 @@ public class MainPangkat08 {
 
         System.out.println("HASIL PANGKAT BRUTEFORCE:");
         for (Pangkat08 p : png) {
-            System.out.println(p.nilai+"^"+p.pangkat+": "+p.pangkatBF(p.nilai, p.pangkat));
+            System.out.println(p.nilai+"^"+p.pangkat+": "+p.pangkatBF());
         }
 
         System.out.println("HASIL PANGKAT DIVIDE AND CONQUER:");
