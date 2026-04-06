730027049bf6ccaa9d461bde3ac0feadff0ad6e7
Galuh-design <pramudyaanantagaluh@gmail.com>
2026-04-06 08:04:09 +0700

    Praktikum 4

--- minggu6/MahasiswaDemo08.java
+++ minggu6/MahasiswaDemo08.java
@@ -32,12 +32,18 @@ public class MahasiswaDemo08 {
         // System.out.println("Data mahasiswa sebelum sorting: ");
         // list.tampil();
 
-        // System.out.println("Data Mahasiswa setelah sorting berdasarkan IPK (DESC) : ");
+        // System.out.println("Data Mahasiswa setelah sorting berdasarkan IPK (DESC) :
+        // ");
         // list.bubbleSort();
         // list.tampil();
 
-        System.out.println("Data yang sudah terurut menggunakan SELECTION SORT (ASC):");
-        list.selectionSort();
+        // System.out.println("Data yang sudah terurut menggunakan SELECTION SORT
+        // (ASC):");
+        // list.selectionSort();
+        // list.tampil();
+
+        System.out.println("Data yang sudah terurut menggunakan INSERTION SORT (ASC)");
+        list.insertionSort();
         list.tampil();
     }
 
