cf1498549cfffde7fd6757e74c2a49bfcb7b8896
Galuh-design <pramudyaanantagaluh@gmail.com>
2026-04-06 09:22:40 +0700

    Modifikasi praktikum 4 pertanyaan1

--- minggu6/MahasiswaBerprestasi08.java
+++ minggu6/MahasiswaBerprestasi08.java
@@ -46,14 +46,14 @@ public class MahasiswaBerprestasi08 {
     }
 
     void insertionSort() {
-    for (int i = 1; i < listMhs.length; i++) {
-        Mahasiswa08 temp = listMhs[i];
-        int j = i;
-        while (j > 0 && listMhs[j - 1].ipk > temp.ipk) {
-            listMhs[j] = listMhs[j - 1];
-            j--;
+        for (int i = 1; i < listMhs.length; i++) {
+            Mahasiswa08 temp = listMhs[i];
+            int j = i;
+            while (j > 0 && listMhs[j - 1].ipk < temp.ipk) {
+                listMhs[j] = listMhs[j - 1];
+                j--;
+            }
+            listMhs[j] = temp;
         }
-        listMhs[j] = temp;
     }
-}
 }
--- minggu6/MahasiswaDemo08.java
+++ minggu6/MahasiswaDemo08.java
@@ -42,7 +42,7 @@ public class MahasiswaDemo08 {
         // list.selectionSort();
         // list.tampil();
 
-        System.out.println("Data yang sudah terurut menggunakan INSERTION SORT (ASC)");
+        System.out.println("Data yang sudah terurut menggunakan INSERTION SORT (DESC)");
         list.insertionSort();
         list.tampil();
     }
