251f7674d1f169aebc028dea5567a6de73812e26
Galuh-design <pramudyaanantagaluh@gmail.com>
2026-04-23 08:37:11 +0700

    Praktikum1

---
+++ minggu9/Mahasiswa08.java
@@ -0,0 +1,19 @@
+package minggu9;
+
+public class Mahasiswa08 {
+    String nama;
+    String nim;
+    String kelas;
+    double nilai;
+
+    public Mahasiswa08(String nama, String nim, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        this.nilai = -1;
+    }
+
+      void tugasDinilai(int nilai) {
+        this.nilai = nilai;
+    }
+}
