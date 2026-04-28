61036cc0f809ad8dfe7d5096791bb788230f0936
Galuh-design <pramudyaanantagaluh@gmail.com>
2026-04-28 08:47:02 +0700

    modifikasi praktikum 2 pertanyaan 2

--- minggu9/StackTugasMahasiswa08.java
+++ minggu9/StackTugasMahasiswa08.java
@@ -74,7 +74,7 @@ public class StackTugasMahasiswa08 {
 
     public String konversiDesimalKeBiner(int nilai) {
         stackKonversi08 stack = new stackKonversi08();
-        while (nilai > 0) {
+        while (nilai != 0) {
             int sisa = nilai % 2;
             stack.push(sisa);
             nilai = nilai / 2;
