74d6fc83b40f733b435a467a9f35d335bf0b0bf0
Galuh-design <pramudyaanantagaluh@gmail.com>
2026-05-05 08:31:24 +0700

    praktikum 2

---
+++ minggu10/Mahasiswa08.java
@@ -0,0 +1,21 @@
+package minggu10;
+
+public class Mahasiswa08 {
+
+    String nim;
+    String nama;
+    String prodi;
+    String kelas;
+
+    public Mahasiswa08(String nim, String nama, String prodi, String kelas) {
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi;
+        this.kelas = kelas;
+    }
+
+    public void tampilkanData() {
+        System.out.println(nim + " " + nama + " " + prodi + " " + kelas);
+    }
+
+}
